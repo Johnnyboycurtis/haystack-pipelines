adae0f7899af838d2dfcac6c4cd0928f9f6d4bb5
johnnyboycurtis <johnnyboycurtis@gmail.com>
2026-02-08 23:34:35 -0800

    environment updates

--- conda_env_setup.sh
+++ conda_env_setup.sh
@@ -1,2 +1,2 @@
 # load `conda activate haystack`
-pip install google-genai haystack-ai langchain-text-splitters ipython jupyter notebook ipykernel tiktoken pinecone-haystack pandas matplotlib
+pip install google-genai haystack-ai langchain-text-splitters ipython jupyter notebook ipykernel tiktoken pinecone-haystack pandas matplotlib google-genai-haystack sentence-transformers black
